66979d4867264f35933a8ffa7b1779a50bfbe0c3
Denys Prokopiuk <prokopiuckdenys250904@gmail.com>
2024-02-24 11:43:55 +0200

    Main_design

--- RealTimeCollaborativeWhiteboard/Controllers/HomeController.cs
+++ RealTimeCollaborativeWhiteboard/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RealTimeCollaborativeWhiteboard.Data;
 using RealTimeCollaborativeWhiteboard.Models;
 using System.Diagnostics;
